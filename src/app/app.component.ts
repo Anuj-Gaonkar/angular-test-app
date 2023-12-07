6eca83ecf200f5e839d8a3298bf6454d06d8de36
Anawarul Haque <anujgaonkar11@gmail.com>
2023-12-07 19:24:46 +0530

    components-styles and global css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+    RouterOutlet, 
+    HomeComponent,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
+  // template: `
+  // <main>
+  //     <header class="brand-name">
+  //       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+  //     </header>
+  //     <section class="content">
+  //       <app-home></app-home>
+  //     </section>
+  //   </main>
+  // `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
