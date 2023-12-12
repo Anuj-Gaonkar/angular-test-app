0a87b661290aef2597f9a41c4b7b6a1b0e304267
Anawarul Haque <anujgaonkar11@gmail.com>
2023-12-12 13:26:29 +0530

    two way binding

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText: string = 'Womens watch';
+  searchText: string = 'New String';
 
   updateSearchText(event: any){
       this.searchText = event.target.value;
